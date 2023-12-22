ea7049892441bcf93267fe9ee13352418bda6ea8
Abel Ortega <abelangel2001@outlook.com>
2023-12-22 14:36:26 -0500

    datos y clases de index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const prompt = require("prompt-sync")();
+const fs = require("fs");
 const pause = () => prompt("(press ENTER to contine...)");
+
+// recover data - users
+//const clients = JSON.parse(
+//  fs.readFileSync("./data/users/clients.json", "utf-8"),
+//);
+//const managers = JSON.parse(
+//  fs.readFileSync("./data/users/managers.json", "utf-8"),
+//);
+//
+//// recover data - products
+//const products = JSON.parse(fs.readFileSync("./data/products.json", "utf-8"));
+
 console.clear();
 
 class user {
@@ -8,13 +21,33 @@ class user {
   #username;
   #email;
   #password;
-  constructor(_name, _lastname, _username, _email, _password) {
+  #rol;
+  constructor(_name, _lastname, _username, _email, _password, _rol) {
     this.#name = _name;
     this.#lastname = _lastname;
     this.#username = _username;
     this.#email = _email;
     this.#password = _password;
+    this.#rol = _rol;
   }
+  // Getters
+  getName() {
+    return this.#name;
+  }
+  getLastname() {
+    return this.#lastname;
+  }
+  getUsername() {
+    return this.#username;
+  }
+  getEmail() {
+    return this.#email;
+  }
+  getPassword() {
+    return this.#password;
+  }
+
+  // Setters
   setName(_name) {
     this.#name = _name;
   }
@@ -30,6 +63,7 @@ class user {
   setPassword(_password) {
     this.#password = _password;
   }
+
   getUser() {
     return {
       name: this.#name,
@@ -37,6 +71,7 @@ class user {
       username: this.#username,
       email: this.#email,
       password: this.#password,
+      rol: this.#rol,
     };
   }
 }
@@ -52,18 +87,34 @@ class product {
     this.#price = _price;
     this.#cant = _cant;
   }
+  // Getters
+  getId() {
+    this.#id;
+  }
+  getName() {
+    this.#name;
+  }
+  getPrice() {
+    this.#price;
+  }
+  getCant() {
+    this.#cant;
+  }
+
+  // Setters
   setId(_id) {
     this.#id = _id;
   }
-  setNombre(_id) {
-    this.#id = _id;
+  setNombre(_name) {
+    this.#name = _name;
   }
-  setId(_id) {
-    this.#id = _id;
+  setPrice(_price) {
+    this.#price = _price;
   }
-  setId(_id) {
-    this.#id = _id;
+  setCant(_cant) {
+    this.#cant = _cant;
   }
+
   getProduct() {
     return {
       id: this.#id,
@@ -74,19 +125,68 @@ class product {
   }
 }
 
-function menu_acceso() {
+//----->const arreglo = [
+//----->  new user("Abel", "Ortega", "aberu029", "abelangel2001@outlook.com", "123", 0),
+//----->  new user(
+//----->    "Angel",
+//----->    "Fuentes",
+//----->    "angel029",
+//----->    "angelfuentes@outlook.com",
+//----->    "123",
+//----->    0,
+//----->  ),
+//----->  new user(
+//----->    "Eduardo",
+//----->    "Holgado",
+//----->    "eduardo029",
+//----->    "eduardoholgado2001@outlook.com",
+//----->    "123",
+//----->    0,
+//----->  ),
+//----->  new user(
+//----->    "Mario",
+//----->    "Escudero",
+//----->    "mario029",
+//----->    "marioescudero2001@outlook.com",
+//----->    "123",
+//----->    0,
+//----->  ),
+//----->];
+//----->
+//----->arreglo.forEach((e) => console.log(e.getName()));
+
+// MENUS
+
+function login_menu() {
+  let bucle = false;
+  let username;
+  let password;
+  while (!bucle) {
+    console.log(
+      `
+       *-------------------------------*
+       |----ONLINE MARKETS - LOG IN----|
+       *-------------------------------*
+      `,
+    );
+    username = prompt(` --> Username: `);
+    password = prompt(` --> Password: `);
+  }
+}
+
+function client_menu() {
   let bucle = false;
   let opcion;
   while (!bucle) {
     console.log(
       `
        *-------------------------------*
-       |--BIENVENIDO A ONLINE MARKETS--|
+       |---WELCOME TO ONLINE MARKETS---|
        *-------------------------------*
-       |     1. Iniciar Sesion         |
-       |     2. Registrarse            |
-       |     3. Creditos               |
-       |     4. Salir                  |                    
+       |     1. Log In                 |
+       |     2. Sign Up                |
+       |     3. Credits                |
+       |     4. Exit                   |                    
        *-------------------------------*
       `,
     );
@@ -117,4 +217,104 @@ function menu_acceso() {
   }
 }
 
-menu_acceso();
+function manager_menu() {
+  let bucle = false;
+  let opcion;
+  while (!bucle) {
+    console.log(
+      `
+       *-------------------------------*
+       |--ONLINE MARKETS - MANAGERS S--|
+       *-------------------------------*
+       |     1. Log In                 |
+       |     2. Recover Account        |
+       |     3. Exit                   |                    
+       *-------------------------------*
+      `,
+    );
+    opcion = parseInt(prompt(` --> `));
+    console.clear();
+    switch (opcion) {
+      case 1:
+        console.log("hola");
+        pause();
+        break;
+      case 2:
+        console.log("hola 2");
+        pause();
+        break;
+      case 3:
+        console.log("salir");
+        pause();
+        bucle = true;
+        break;
+      default:
+        break;
+    }
+    console.clear();
+  }
+}
+
+function access_menu() {
+  let bucle = false;
+  let opcion;
+  while (!bucle) {
+    console.log(
+      `
+       *-------------------------------*
+       |---ONLINE MARKETS - ACCESS ----|
+       *-------------------------------*
+       |     1. Managers Access        |
+       |     2. Clients Access         |
+       |     3. Exit                   |                    
+       *-------------------------------*
+      `,
+    );
+    opcion = parseInt(prompt(` --> `));
+    console.clear();
+    switch (opcion) {
+      case 1:
+        console.log("hola");
+        pause();
+        break;
+      case 2:
+        console.log("hola 2");
+        pause();
+        break;
+      case 3:
+        console.log("salir");
+        pause();
+        bucle = true;
+        break;
+      default:
+        break;
+    }
+    console.clear();
+  }
+}
+
+// test
+// leer archivos
+//const fs = require("fs");
+//const datos = fs.readFileSync("./data/users/clients.json", "utf-8");
+//const personas = JSON.parse(datos);
+////console.log(personas);
+//
+//// escritura
+//const persona = {
+//  name: "Leonardo",
+//  lastname: "Gabriel",
+//  username: "Leo",
+//  email: "leonardo29@outlook.com",
+//  password: "clavekdkd3",
+//  rol: 0,
+//};
+//personas.push(persona);
+//const jsonData = JSON.stringify(personas, null, 2);
+//fs.writeFileSync("./data/users/clients.json", jsonData, (error) => {
+//  if (error) {
+//    console.log(`error: ${error}`);
+//  } else {
+//    console.log("Archivo json generado correctamente");
+//  }
+//});
